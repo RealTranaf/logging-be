761236bd0fc70250c4342193a3a44d914053e767
Trần Linh Duy <duytranlinh0@gmail.com>
2026-03-24 14:34:35 +0700

    Update

--- src/main/java/com/vtc/logging/controller/LogController.java
+++ src/main/java/com/vtc/logging/controller/LogController.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:3000")
++@CrossOrigin(origins = {"http://localhost:3000","http://35.222.18.171:3000"})
 
 @RestController
 @RequestMapping("/logs")
